3ad6412eb835b16e805632f6961361085b30d753
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-07-20 21:38:11 +0100

    created basic shell

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+
+#define SHELL_H
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS 10
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#endif /* SHELL_H */
